8aedeb23657df3566b5d59a2a4bde0deebc921b8
Katie Huang <kofanhuang@gmail.com>
2021-03-13 11:21:57 -0800

    Add lp_cat, lp_whole to each saved frame

--- OpenCV_blur/detect_blur_frames.py
+++ OpenCV_blur/detect_blur_frames.py
@@ -8,12 +8,13 @@ from os.path import isfile, join
 # Bad frames
 # Read all files in folder as a list
 mypath = '/Users/katiehuang/Documents/best_cat/OpenCV_blur/frame_bad_rename/'
-files = sorted([f for f in listdir(mypath) if isfile(join(mypath, f))])
+# files = sorted([f for f in listdir(mypath) if isfile(join(mypath, f))])
+files = sorted([f for f in listdir(mypath) if f!='.DS_Store'])
 # print('Number of files:', len(files))
 # print(files)
 
 # Headers (=column names for df)
-headers = ['filename','lp_ratio','blur']
+headers = ['filename','lp_cat','lp_all','lp_ratio','blur']
 # Empty list to record each row (file)
 # Will be a list of lists
 rows = []
@@ -23,22 +24,34 @@ rows = []
 # Empty list to record blurry or not (0: clear, 1: blurry)
 # blur = []
 # Read file
-for file in files[:]:
+for file in files[0:]:
     print(file)
     img = cv.imread(mypath+file, cv.IMREAD_GRAYSCALE)
 
     # Detect cat face
     faces_rect = haar_cascade.detectMultiScale(img, scaleFactor=1.1, minNeighbors=3)
     # Draw rectangle around detected cat face
-    for (x,y,w,h) in faces_rect:
+    # print(len(faces_rect))
+    if len(faces_rect) > 0:
+        for (x,y,w,h) in faces_rect:
+            cv.rectangle(img, (x,y), (x+w,y+h), (0,255,0), thickness=3)
+
+        (x,y,w,h) = faces_rect[0]
+
+    else:
+        (x,y,w,h) = (0,0,1,1)
         cv.rectangle(img, (x,y), (x+w,y+h), (0,255,0), thickness=3)
+
 
     cv.imshow('Detected Cat Faces', img)
 
     # Crop and leave cat face only
-    (x,y,w,h) = faces_rect[0]
+    # (x,y,w,h) = faces_rect[0]
     cropped = img[y:y+h,x:x+w]
     cv.imshow('Cropped',cropped)
+    kernel_size=3;
+    laplacian_var_roi = cv.Laplacian(cropped, cv.CV_64F,kernel_size).var()
+    # print("ROI:",laplacian_var_roi)
     # cropped_gray = cv.cvtColor(cropped, cv.IMREAD_GRAYSCALE)
     # print(w,h)
     # print(img.shape)
@@ -47,22 +60,19 @@ for file in files[:]:
 
     resized = cv.resize(cropped, (w*scale,h*scale), interpolation=cv.INTER_CUBIC)
     cv.imshow('Resized',resized)
-    kernel_size=3;
-    laplacian_var_roi_resized = cv.Laplacian(resized, cv.CV_64F,kernel_size).var()
+    lp_cat = cv.Laplacian(resized, cv.CV_64F,kernel_size).var()
     # print("ROI resized:",laplacian_var_roi_resized)
 
-    laplacian_var_roi = cv.Laplacian(cropped, cv.CV_64F,kernel_size).var()
-    # print("ROI:",laplacian_var_roi)
-    laplacian_var_whole = cv.Laplacian(img, cv.CV_64F,kernel_size).var()
+    lp_all = cv.Laplacian(img, cv.CV_64F,kernel_size).var()
     # print("Whole:",laplacian_var_whole)
-    lp_ratio = laplacian_var_roi_resized/laplacian_var_whole
+    lp_ratio = lp_cat/lp_all
     # lp_ratio.append(laplacian_var_roi_resized/laplacian_var_whole)
     # print("ROI/Whole:",laplacian_var_roi/laplacian_var_whole)
     # print("ROIr/Whole:",laplacian_var_roi_resized/laplacian_var_whole)
 
 
     # Check if blurry
-    if laplacian_var_roi_resized/laplacian_var_whole < 0.03:
+    if lp_cat/lp_all < 0.03:
         blur = 1
         # blur.append(1)
         # print("Image blurry")
@@ -71,7 +81,7 @@ for file in files[:]:
         # blur.append(0)
         # print("Image okay")
 
-    rows.append([file,lp_ratio,blur])
+    rows.append([file,lp_cat,lp_all,lp_ratio,blur])
 
     cv.waitKey(1)
 
@@ -93,7 +103,7 @@ files = sorted([f for f in listdir(mypath) if isfile(join(mypath, f))])
 # print(files)
 
 # Headers (=column names for df)
-headers = ['filename','lp_ratio','blur']
+headers = ['filename','lp_cat','lp_all','lp_ratio','blur']
 # Empty list to record each row (file)
 # Will be a list of lists
 rows = []
@@ -116,9 +126,12 @@ for file in files[:]:
     cv.imshow('Detected Cat Faces', img)
 
     # Crop and leave cat face only
-    (x,y,w,h) = faces_rect[0]
+    # (x,y,w,h) = faces_rect[0]
     cropped = img[y:y+h,x:x+w]
     cv.imshow('Cropped',cropped)
+    kernel_size=3;
+    laplacian_var_roi = cv.Laplacian(cropped, cv.CV_64F,kernel_size).var()
+    # print("ROI:",laplacian_var_roi)
     # cropped_gray = cv.cvtColor(cropped, cv.IMREAD_GRAYSCALE)
     # print(w,h)
     # print(img.shape)
@@ -127,22 +140,19 @@ for file in files[:]:
 
     resized = cv.resize(cropped, (w*scale,h*scale), interpolation=cv.INTER_CUBIC)
     cv.imshow('Resized',resized)
-    kernel_size=3;
-    laplacian_var_roi_resized = cv.Laplacian(resized, cv.CV_64F,kernel_size).var()
+    lp_cat = cv.Laplacian(resized, cv.CV_64F,kernel_size).var()
     # print("ROI resized:",laplacian_var_roi_resized)
 
-    laplacian_var_roi = cv.Laplacian(cropped, cv.CV_64F,kernel_size).var()
-    # print("ROI:",laplacian_var_roi)
-    laplacian_var_whole = cv.Laplacian(img, cv.CV_64F,kernel_size).var()
+    lp_all = cv.Laplacian(img, cv.CV_64F,kernel_size).var()
     # print("Whole:",laplacian_var_whole)
-    lp_ratio = laplacian_var_roi_resized/laplacian_var_whole
+    lp_ratio = lp_cat/lp_all
     # lp_ratio.append(laplacian_var_roi_resized/laplacian_var_whole)
     # print("ROI/Whole:",laplacian_var_roi/laplacian_var_whole)
     # print("ROIr/Whole:",laplacian_var_roi_resized/laplacian_var_whole)
 
 
     # Check if blurry
-    if laplacian_var_roi_resized/laplacian_var_whole < 0.03:
+    if lp_cat/lp_all < 0.03:
         blur = 1
         # blur.append(1)
         # print("Image blurry")
@@ -151,7 +161,7 @@ for file in files[:]:
         # blur.append(0)
         # print("Image okay")
 
-    rows.append([file,lp_ratio,blur])
+    rows.append([file,lp_cat,lp_all,lp_ratio,blur])
 
     cv.waitKey(1)
 
